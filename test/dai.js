c2bdbbbd4bd3ac0e13bb0475b1389d1cfc3e6aa8
Ryan Ghods <ryan@ryanio.com>
2021-01-24 14:39:39 -0800

    updates, simplification

--- test/dai.js
+++ test/dai.js
@@ -1,30 +1,31 @@
-const { BN, ether, balance } = require('openzeppelin-test-helpers');
 const { expect } = require('chai');
-const { asyncForEach } = require('./utils');
-
-// ABI
+const { BN, ether, balance } = require('@openzeppelin/test-helpers');
 const daiABI = require('./abi/dai');
 
-// userAddress must be unlocked using --unlock ADDRESS
-const userAddress = '0x9eb7f2591ed42dee9315b6e2aaf21ba85ea69f8c';
+// userAddress and daiAddress must be unlocked in ganache-cli using --unlock
+const userAddress = '0x7566126f2fd0f2dddae01bb8a6ea49b760383d5a';
 const daiAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
 const daiContract = new web3.eth.Contract(daiABI, daiAddress);
 
-contract('Truffle Mint DAI', async accounts => {
+contract('Truffle Mint DAI', async (accounts) => {
   it('should send ether to the DAI address', async () => {
     // Send 0.1 eth to userAddress to have gas to send an ERC20 tx.
     await web3.eth.sendTransaction({
       from: accounts[0],
       to: userAddress,
-      value: ether('0.1')
+      value: ether('0.1'),
     });
     const ethBalance = await balance.current(userAddress);
     expect(new BN(ethBalance)).to.be.bignumber.least(new BN(ether('0.1')));
   });
 
   it('should mint DAI for our first 5 generated accounts', async () => {
-    // Get 100 DAI for first 5 accounts
-    await asyncForEach(accounts.slice(0, 5), async account => {
+    // Verify dai balance
+    const daiBalance = await daiContract.methods.balanceOf(userAddress).call();
+    expect(new BN(daiBalance)).to.be.bignumber.least(ether('500'));
+
+    // Send 100 DAI to the first 5 accounts
+    for (const account of accounts.slice(0, 5)) {
       // daiAddress is passed to ganache-cli with flag `--unlock`
       // so we can use the `transfer` method
       await daiContract.methods
@@ -32,6 +33,6 @@ contract('Truffle Mint DAI', async accounts => {
         .send({ from: userAddress, gasLimit: 800000 });
       const daiBalance = await daiContract.methods.balanceOf(account).call();
       expect(new BN(daiBalance)).to.be.bignumber.least(ether('100'));
-    });
+    }
   });
 });
--- test/sai.js
+++ test/sai.js
@@ -1,23 +1,13 @@
-const { BN, ether, balance } = require('openzeppelin-test-helpers');
 const { expect } = require('chai');
-const { asyncForEach } = require('./utils');
-
-// Artifacts
+const { BN, ether, balance } = require('@openzeppelin/test-helpers');
+const erc20ABI = require('./abi/erc20');
 const ForceSend = artifacts.require('ForceSend');
 
-// ABI
-const erc20ABI = require('./abi/erc20');
-
-// saiAddress must be unlocked using --unlock 0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359
+// saiAddress must be unlocked in ganache-cli using --unlock ADDRESS
 const saiAddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359';
 const saiContract = new web3.eth.Contract(erc20ABI, saiAddress);
 
-// Utils
-const getSaiBalance = async account => {
-  return saiContract.methods.balanceOf(account).call();
-};
-
-contract('Truffle Mint SAI', async accounts => {
+contract('Truffle Mint SAI', async (accounts) => {
   it('should send ether to the SAI contract', async () => {
     // Send 1 eth to saiAddress to have gas to mint.
     // Uses ForceSend contract, otherwise just sending
@@ -29,15 +19,15 @@ contract('Truffle Mint SAI', async accounts => {
   });
 
   it('should mint SAI for our first 5 generated accounts', async () => {
-    // Get 100 SAI for first 5 accounts
-    await asyncForEach(accounts.slice(0, 5), async account => {
+    // Mint 100 SAI for the first 5 accounts
+    for (const account of accounts.slice(0, 5)) {
       // saiAddress is passed to ganache-cli with flag `--unlock`
       // so we can use the `mint` method.
       await saiContract.methods
         .mint(account, ether('100').toString())
         .send({ from: saiAddress });
-      const saiBalance = await getSaiBalance(account);
+      const saiBalance = await saiContract.methods.balanceOf(account).call();
       expect(new BN(saiBalance)).to.be.bignumber.least(ether('100'));
-    });
+    }
   });
 });
